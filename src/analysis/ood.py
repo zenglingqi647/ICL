ee74803112eb9010ebb19b3ab235538332e56e48
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-06 03:53:34 +0800

    ood plot

--- src/analysis/ood.py
+++ src/analysis/ood.py
@@ -9,10 +9,7 @@ from eval import get_run_metrics, read_run_dir, get_model_from_run
 from tasks import get_task_sampler
 
 
-def ood_eval(model,
-                conf,run_path,
-                n_points=range(10, 80, 10),
-                batch_size=64):
+def ood_eval(model, conf, run_path, n_points=range(10, 90, 10), batch_size=64):
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
@@ -21,39 +18,30 @@ def ood_eval(model,
     n_dims = conf.model.n_dims
     batch_size = conf.training.batch_size
 
-    fig, ax = plt.subplots()
-    
-    plt.title("Robustness to Query Scales")
-    plt.xlabel("scale")
+    plt.title(conf.training.train_test_dist)
+    plt.xlabel("n_points")
     plt.ylabel("accuracy")
-    ax.set_xscale('log')
 
+    results = []
     for num_ex in tqdm(n_points):
-        results = []
-        
+
         task_sampler = get_task_sampler(conf.training.task, n_dims, batch_size, **conf.training.task_kwargs)
         task = task_sampler()
-        
+
         xs = np.load(os.path.join(run_path, 'test_xs.npy'))
+        xs = torch.from_numpy(xs[:, :num_ex, :])
         ys = task.evaluate(xs)
-        
+
         with torch.no_grad():
             pred = model(xs.to(device), ys.to(device))
             pred = pred.cpu()
         metric = task.get_metric()
         acc = metric(pred, ys).numpy()
         results.append(acc.mean())
-        
-        plt.plot(n_points, results, marker='o', linewidth=2, label=f"{num_ex} examples")
-    
-    plt.legend()
-    plt.savefig("figs/ood_accuracy.png", bbox_inches='tight')
 
-    # Plotting
-    # for n_dims, losses in results.items():
-    #     for i, loss in enumerate(losses):
-    #         plt.plot(loss, lw=2, label=f"n_dims={n_dims}, scale={scales[i]}")
-    
+    plt.plot(list(n_points), results, marker='o', linewidth=2, label="Accuracy")
+    plt.legend()
+    plt.savefig(f"figs/ood_{conf.training.train_test_dist}.png", bbox_inches='tight')
 
 
 if __name__ == "__main__":
@@ -64,7 +52,7 @@ if __name__ == "__main__":
 
     df = read_run_dir(run_dir)
     task = "logistic_regression"
-    run_id = "trained_partial"  # if you train more models, replace with the run_id from the table above
+    run_id = "04dd40ac-0a8d-43de-89e3-f926b7085d46"  # if you train more models, replace with the run_id from the table above
 
     run_path = os.path.join(run_dir, task, run_id)
     recompute_metrics = False
