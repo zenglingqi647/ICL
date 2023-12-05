0237391901342798855e4ec42e57f0d20d232f0f
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-06 04:00:44 +0800

    update configs

--- scripts/ood.sh
+++ scripts/ood.sh
@@ -1,10 +1,18 @@
 tmux
+tmux attach -t 
 
 cd /data1/lzengaf/cs182/ICL/src
 conda activate in-context-learning
 export CUDA_VISIBLE_DEVICES=
 
-python train.py --config conf/ood/None.yaml
-python train.py --config conf/ood/normal.yaml
-python train.py --config conf/ood/permute.yaml
-python train.py --config conf/ood/uniform.yaml
+python train.py --config conf/ood/standard.yaml
+python train.py --config conf/ood/opposite.yaml
+python train.py --config conf/ood/random.yaml
+python train.py --config conf/ood/orthogonal.yaml
+python train.py --config conf/ood/overlapping.yaml
+
+python train.py --config conf/ood/standard_rbf.yaml
+python train.py --config conf/ood/opposite_rbf.yaml
+python train.py --config conf/ood/random_rbf.yaml
+python train.py --config conf/ood/orthogonal_rbf.yaml
+python train.py --config conf/ood/overlapping_rbf.yaml
---
+++ scripts/randlb.sh
@@ -0,0 +1,15 @@
+tmux
+
+cd /data1/lzengaf/cs182/ICL/src
+conda activate in-context-learning
+export CUDA_VISIBLE_DEVICES=
+
+python train.py --config conf/randlb/None.yaml
+python train.py --config conf/randlb/normal.yaml
+python train.py --config conf/randlb/permute.yaml
+python train.py --config conf/randlb/uniform.yaml
+
+python train.py --config conf/randlb/None_rbf.yaml
+python train.py --config conf/randlb/normal_rbf.yaml
+python train.py --config conf/randlb/permute_rbf.yaml
+python train.py --config conf/randlb/uniform_rbf.yaml
