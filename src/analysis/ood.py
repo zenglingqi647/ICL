3476b9608f83a6a16eeb988bca930130cffaa126
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-06 17:06:09 +0800

    update ood plot

--- src/analysis/ood.py
+++ src/analysis/ood.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 from eval import get_run_metrics, read_run_dir, get_model_from_run
 from tasks import get_task_sampler
-
+from samplers import get_data_sampler
 
 def ood_eval(model, conf, run_path, n_points=range(10, 90, 10), batch_size=64):
 
@@ -27,7 +27,14 @@ def ood_eval(model, conf, run_path, n_points=range(10, 90, 10), batch_size=64):
 
         task_sampler = get_task_sampler(conf.training.task, n_dims, batch_size, **conf.training.task_kwargs)
         task = task_sampler()
-
+        
+        xs_path = os.path.join(run_path, 'test_xs.npy')
+        if not os.path.exists(xs_path):
+            if 'standard' not in conf.training.task:
+                raise ValueError('Missing test_xs.npy for OOD task!')
+            data_sampler = get_data_sampler(conf.training.data, n_dims)
+            xs = data_sampler.sample_xs(b_size=batch_size, n_points=num_ex)
+            
         xs = np.load(os.path.join(run_path, 'test_xs.npy'))
         xs = torch.from_numpy(xs[:, :num_ex, :])
         ys = task.evaluate(xs)
@@ -41,24 +48,29 @@ def ood_eval(model, conf, run_path, n_points=range(10, 90, 10), batch_size=64):
 
     plt.plot(list(n_points), results, marker='o', linewidth=2, label="Accuracy")
     plt.legend()
-    plt.savefig(f"figs/ood_{conf.training.train_test_dist}.png", bbox_inches='tight')
+    plt.savefig(f"figs/{conf.training.task}_{conf.training.train_test_dist}.png", bbox_inches='tight')
 
 
 if __name__ == "__main__":
     sns.set_theme('notebook', 'darkgrid')
     palette = sns.color_palette('colorblind')
 
-    run_dir = "/data1/lzengaf/cs182/ICL/models"
+    run_dir = "/data1/lzengaf/cs182/ICL/models/"
+    task = 'logistic_regression_ood'
 
-    df = read_run_dir(run_dir)
-    task = "logistic_regression"
-    run_id = "04dd40ac-0a8d-43de-89e3-f926b7085d46"  # if you train more models, replace with the run_id from the table above
+    assert 'ood' in task, 'Please specify an OOD task!'
 
-    run_path = os.path.join(run_dir, task, run_id)
-    recompute_metrics = False
+    df = read_run_dir(os.path.join(run_dir, task))
 
-    if recompute_metrics:
-        get_run_metrics(run_path)  # these are normally precomputed at the end of training
-    model, conf = get_model_from_run(run_path)
+    for run_id in tqdm(os.path.join(run_dir, task)):
+        if not os.path.isdir(os.path.join(run_dir, task, run_id)):
+            continue
+        
+        run_path = os.path.join(run_dir, task, run_id)
+        recompute_metrics = False
 
-    ood_eval(model, conf, run_path)
+        if recompute_metrics:
+            get_run_metrics(run_path)  # these are normally precomputed at the end of training
+        model, conf = get_model_from_run(run_path)
+
+        ood_eval(model, conf, run_path)
