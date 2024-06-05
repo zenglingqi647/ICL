5ac0651ac51c649ee7c00aa95588f4eba231cbc6
zenglingqi647 <lzengaf@connect.ust.hk>
2024-06-05 15:11:23 +0800

    baseline unit test

---
+++ src/test/baseline_unit_test.py
@@ -0,0 +1,235 @@
+import json
+from tqdm import tqdm
+import torch
+import models
+from samplers import get_data_sampler, sample_transformation
+from tasks import get_task_sampler
+from omegaconf import OmegaConf
+
+
+def eval_batch(model, task_sampler, xs, xs_p=None):
+    task = task_sampler()
+    if torch.cuda.is_available() and model.name.split("_")[0] in ["gpt2", "lstm"]:
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    if xs_p is None:
+        ys = task.evaluate(xs)
+        pred = model(xs.to(device), ys.to(device)).detach()
+        metrics = task.get_metric()(pred.cpu(), ys)
+    else:
+        b_size, n_points, _ = xs.shape
+        metrics = torch.zeros(b_size, n_points)
+        for i in range(n_points):
+            xs_comb = torch.cat((xs[:, :i, :], xs_p[:, i:, :]), dim=1)
+            ys = task.evaluate(xs_comb)
+
+            pred = model(xs_comb.to(device), ys.to(device), inds=[i]).detach()
+            metrics[:, i] = task.get_metric()(pred.cpu(), ys)[:, i]
+
+    return metrics
+
+
+def aggregate_metrics(metrics, bootstrap_trials=1000):
+    """
+    Takes as input a tensor of shape (num_eval, n_points) and returns a dict with
+    per-point mean, stddev, and bootstrap limits.
+    """
+    results = {}
+    results["mean"] = metrics.mean(dim=0)
+    results["std"] = metrics.std(dim=0, unbiased=True)
+    n = len(metrics)
+    bootstrap_indices = torch.randint(n, size=(bootstrap_trials, n))
+    bootstrap_means = metrics[bootstrap_indices].mean(dim=1).sort(dim=0)[0]
+    results["bootstrap_low"] = bootstrap_means[int(0.05 * bootstrap_trials), :]
+    results["bootstrap_high"] = bootstrap_means[int(0.95 * bootstrap_trials), :]
+
+    return {k: v.tolist() for k, v in results.items()}
+
+
+def eval_model(
+    model,
+    task_name,
+    data_name,
+    n_dims,
+    n_points,
+    prompting_strategy,
+    num_eval_examples=1280,
+    batch_size=64,
+    data_sampler_kwargs={},
+    task_sampler_kwargs={},
+):
+    """
+    Evaluate a model on a task with a variety of strategies.
+       Args:
+       - task: which base task we are evaluating on. E.g., "linear_regression"
+       - prompting_strategy: how to construct the prompt, e.g., "random_quadrants"
+       - num_eval_examples: total number of examples to evaluate on
+       - **sampler_kwargs: remaining arguments to pass directly to the sampler
+    """
+
+    assert num_eval_examples % batch_size == 0
+    data_sampler = get_data_sampler(data_name, n_dims, **data_sampler_kwargs)
+    task_sampler = get_task_sampler(task_name, n_dims, batch_size, **task_sampler_kwargs)
+
+    all_metrics = []
+
+    generating_func = globals()[f"gen_{prompting_strategy}"]
+    for i in range(num_eval_examples // batch_size):
+        xs, xs_p = generating_func(data_sampler, n_points, batch_size)
+
+        metrics = eval_batch(model, task_sampler, xs, xs_p)
+        all_metrics.append(metrics)
+
+    metrics = torch.cat(all_metrics, dim=0)
+
+    return aggregate_metrics(metrics)
+
+
+def build_evals(conf):
+    n_dims = conf.model.n_dims
+    n_points = conf.training.curriculum.points.end
+    batch_size = conf.training.batch_size
+
+    task_name = conf.training.task
+    data_name = conf.training.data
+
+    base_kwargs = {
+        "task_name": task_name,
+        "n_dims": n_dims,
+        "n_points": n_points,
+        "batch_size": batch_size,
+        "data_name": data_name,
+        "prompting_strategy": "standard",
+    }
+
+    if len(conf.training.task_kwargs) > 0:
+        base_kwargs["task_sampler_kwargs"] = conf.training.task_kwargs
+
+    evaluation_kwargs = {}
+
+    evaluation_kwargs["standard"] = {"prompting_strategy": "standard"}
+    if task_name != "linear_regression":
+        if task_name in ["relu_2nn_regression"]:
+            evaluation_kwargs["linear_regression"] = {"task_name": "linear_regression"}
+        for name, kwargs in evaluation_kwargs.items():
+            # allow kwargs to override base_kwargs values
+            evaluation_kwargs[name] = base_kwargs.copy()
+            evaluation_kwargs[name].update(kwargs)
+        return evaluation_kwargs
+
+    for strategy in ["opposite_orthant", "random_orthant", "orthogonal", "projection", "expansion"]:
+        evaluation_kwargs[strategy] = {"prompting_strategy": strategy}
+
+    for method in ["half_subspace", "skewed"]:
+        if "subspace" in method:
+            eigenvals = torch.zeros(n_dims)
+            eigenvals[:n_dims // 2] = 1
+        else:
+            eigenvals = 1 / (torch.arange(n_dims) + 1)
+
+        scale = sample_transformation(eigenvals, normalize=True)
+        evaluation_kwargs[f"{method}"] = {
+            "data_sampler_kwargs": {
+                "scale": scale
+            },
+        }
+
+    for dim in ["x", "y"]:
+        for scale in [0.333, 0.5, 2, 3]:
+            if dim == "x":
+                eigenvals = scale * torch.ones(n_dims)
+                t = sample_transformation(eigenvals)
+                scaling_args = {"data_sampler_kwargs": {"scale": t}}
+            else:
+                eigenvals = scale * torch.ones(n_dims)
+                scaling_args = {"task_sampler_kwargs": {"scale": scale}}
+
+            evaluation_kwargs[f"scale-{dim}={scale}"] = scaling_args
+
+    evaluation_kwargs[f"noisyLR"] = {
+        "task_sampler_kwargs": {
+            "renormalize_ys": True,
+            "noise_std": 1
+        },
+        "task_name": "noisy_linear_regression",
+    }
+
+    for name, kwargs in evaluation_kwargs.items():
+        # allow kwargs to override base_kwargs values
+        evaluation_kwargs[name] = base_kwargs.copy()
+        evaluation_kwargs[name].update(kwargs)
+
+    return evaluation_kwargs
+
+
+def compute_evals(all_models, evaluation_kwargs, save_path=None):
+    try:
+        with open(save_path) as fp:
+            all_metrics = json.load(fp)
+    except Exception:
+        all_metrics = {}
+
+    for eval_name, kwargs in tqdm(evaluation_kwargs.items()):
+        metrics = {}
+        for model in all_models:
+            metrics[model.name] = eval_model(model, **kwargs)
+        all_metrics[eval_name] = metrics
+
+    if save_path is not None:
+        with open(save_path, "w") as fp:
+            json.dump(all_metrics, fp, indent=2)
+
+    return all_metrics
+
+
+def baseline_names(name):
+    if "OLS" in name:
+        return "Least Squares"
+    if name == "averaging":
+        return "Averaging"
+    if "NN" in name:
+        k = name.split("_")[1].split("=")[1]
+        if "RBFNN" in name:
+            return f"{k}-Nearest Neighbors (RBF)"
+        return f"{k}-Nearest Neighbors"
+    if "lasso" in name:
+        alpha = name.split("_")[1].split("=")[1]
+        return f"Lasso (alpha={alpha})"
+    if "gd" in name:
+        return "2-layer NN, GD"
+    if "decision_tree" in name:
+        return "Greedy Tree Learning"
+    if "xgboost" in name:
+        return "XGBoost"
+    if "svm" in name:
+        if "rbf" in name:
+            return "SVM (RBF)"
+        return "SVM"
+    if "gaussian_process_classifier" in name:
+        if "rbf" in name:
+            return "Gaussian Process Classifier (RBF)"
+        return "Gaussian Process Classifier"
+    if name == "lda":
+        return "LDA"
+    if "logistic_regression" in name:
+        if "rbf" in name:
+            return "Logistic Regression (RBF)"
+        return "Logistic Regression"
+    return name
+
+
+def get_run_metrics(config_path, save_path):
+    conf = OmegaConf.load(config_path)
+    all_models = []
+    all_models += models.get_relevant_baselines(conf.training.task)
+    evaluation_kwargs = build_evals(conf)
+    all_metrics = compute_evals(all_models, evaluation_kwargs, save_path)
+    return all_metrics
+
+
+if __name__ == "__main__":
+    config_path = ""
+    save_path = ""
+    metrics = get_run_metrics(config_path, save_path)
